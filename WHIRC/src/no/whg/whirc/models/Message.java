026da0d711dba163eb725808e60caebe8a148b49
Snorre Brecheisen <snott_on_a_roll@hotmail.com>
2013-11-20 22:09:27 +0100

    or did i

---
+++ WHIRC/src/no/whg/whirc/models/Message.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package no.whg.whirc.models;
+
+/**
+ * @author Fredrik
+ *
+ */
+public class Message {
+	String name;
+	String message;
+	String time;
+	
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	/**
+	 * @return the time
+	 */
+	public String getTime() {
+		return time;
+	}
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+
+}
