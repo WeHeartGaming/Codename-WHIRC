ee408113ca76ac828902696d352d094a6fb72de5
Adhenedhel <peerandreas@live.com>
2013-11-22 20:32:24 +0100

    Added the db class

---
+++ WHIRC/src/no/whg/whirc/models/WhircDB.java
@@ -0,0 +1,42 @@
+package no.whg.whirc.models;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class WhircDB extends SQLiteOpenHelper {
+	
+	public static final String WHIRC_TABLE = "whircTable";
+	public static final String COLUMNID = "id";
+	public static final String COLUMN_TEST = "test";
+	
+	private static final String DATABASE_NAME = "whirc.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	
+	private static final String  DATABASE_CREATE = "Create table" 
+			+ WHIRC_TABLE + "(" + COLUMNID
+			+ " integer primary key autoincrement, " + COLUMN_TEST
+			+ " text not null);";
+	
+	public WhircDB(Context context)
+	{
+		super(context, DATABASE_NAME , null, DATABASE_VERSION);
+	}
+	
+	
+	public void onCreate(SQLiteDatabase database)
+	{
+		database.execSQL(DATABASE_CREATE);
+	}
+	
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+		Log.w(WhircDB.class.getName(), "Upgreading database from version " + oldVersion + " to "
+				+ newVersion + " , this will destroy old data");
+		db.execSQL("DROP TABLE IF EXISTS " + WHIRC_TABLE);
+		onCreate(db);
+	}
+
+}
