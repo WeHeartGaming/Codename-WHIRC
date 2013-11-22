7721d3c8541f172bc3e791c9c68c4ac1507f2703
Adhenedhel <peerandreas@live.com>
2013-11-22 20:59:38 +0100

    Added some more columns to the tables

--- WHIRC/src/no/whg/whirc/models/db/WhircDB.java
+++ WHIRC/src/no/whg/whirc/models/db/WhircDB.java
@@ -9,7 +9,12 @@ public class WhircDB extends SQLiteOpenHelper {
 	
 	public static final String WHIRC_TABLE = "whircTable";
 	public static final String COLUMNID = "id";
-	public static final String COLUMN_TEST = "test";
+	public static final String COLUMN_CHANNELNAME = "channelName";
+	public static final String COLUMN_CHANNELSERVER = "channelServer";
+	public static final String COLUMN_CHANNELPASS = "channelPass";
+	public static final String COLUMN_SERVER = "serverName";
+	
+	public static final String USER_TABLE = "userTable";
 	
 	private static final String DATABASE_NAME = "whirc.db";
 	private static final int DATABASE_VERSION = 1;
@@ -17,8 +22,11 @@ public class WhircDB extends SQLiteOpenHelper {
 	
 	private static final String  DATABASE_CREATE = "Create table" 
 			+ WHIRC_TABLE + "(" + COLUMNID
-			+ " integer primary key autoincrement, " + COLUMN_TEST
-			+ " text not null);";
+			+ " integer primary key autoincrement, " + COLUMN_CHANNELNAME
+			+ " text not null, " + COLUMN_CHANNELSERVER
+			+ " text not null, " + COLUMN_SERVER
+			+ " text not null, " + COLUMN_CHANNELPASS
+			+ " text);";
 	
 	public WhircDB(Context context)
 	{
