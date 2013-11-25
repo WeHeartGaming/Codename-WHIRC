de34df6cb010874ce1647e9ba7aea855ea2318eb
Adhenedhel <peerandreas@live.com>
2013-11-25 23:38:40 +0100

    Added hostname to the server table and added the new entries to the server-object

--- WHIRC/src/no/whg/whirc/models/db/Server.java
+++ WHIRC/src/no/whg/whirc/models/db/Server.java
@@ -3,6 +3,10 @@ package no.whg.whirc.models.db;
 public class Server {
 	private long id;
 	private String server;
+	private String host;
+	private String password;
+	private String channelServer;
+	private String channelName;
 	
 	public long getId() {
 		return id;
@@ -24,4 +28,46 @@ public class Server {
 	public String toString() {
 		return server;
 	}
+	
+	public void setHost(String host)
+	{
+		this.host = host;
+	}
+	
+	public String getHost()
+	{
+		return host;
+	}
+	
+	public void setPasswordf(String password)
+	{
+		this.password = password;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	public void setChannelServer(String channelServer)
+	{
+		this.channelServer = channelServer;
+	}
+	
+	public String getChannelServer()
+	{
+		return channelServer;
+	}
+	
+	public void setChannelName(String channelName)
+	{
+		this.channelName = channelName;
+	}
+	
+	public String getChannelName()
+	{
+		return channelName;
+	}
+	
+	
 }
--- WHIRC/src/no/whg/whirc/models/db/WhircDB.java
+++ WHIRC/src/no/whg/whirc/models/db/WhircDB.java
@@ -7,13 +7,21 @@ import android.util.Log;
 
 public class WhircDB extends SQLiteOpenHelper {
 	
+	/**
+	 *  The table data for the WHIRC_TABLE
+	 */
 	public static final String WHIRC_TABLE = "whircTable";
 	public static final String COLUMNID = "id";
 	public static final String COLUMN_CHANNELNAME = "channelName";
 	public static final String COLUMN_CHANNELSERVER = "channelServer";
 	public static final String COLUMN_CHANNELPASS = "channelPass";
 	public static final String COLUMN_SERVER = "serverName";
+	public static final String COLUMN_HOSTNAME = "hostName";
 	
+	/**
+	 * 
+	 *  The table data for the USER_TABLE
+	 */
 	public static final String USER_TABLE = "userTable";
 	public static final String USER_COLUMNID = "id";
 	public static final String COLUMN_USERONE = "userNameOne";
@@ -28,28 +36,41 @@ public class WhircDB extends SQLiteOpenHelper {
 	
 	
 	private static final String  DATABASE_CREATE = "Create table " 
-			+ WHIRC_TABLE + "(" + COLUMNID
-			+ " integer primary key autoincrement, " + COLUMN_CHANNELNAME
-			+ " text not null, " + COLUMN_CHANNELSERVER
-			+ " text not null, " + COLUMN_SERVER
-			+ " text not null, " + COLUMN_CHANNELPASS
-			+ " text);";
-	
+			+ WHIRC_TABLE + "("
+			+ COLUMNID + " integer primary key autoincrement, " 
+			+ COLUMN_CHANNELNAME + " text not null, "
+			+ COLUMN_CHANNELSERVER + " text not null, "
+			+ COLUMN_SERVER + " text not null, "
+			+ COLUMN_CHANNELPASS + " text,"
+			+ COLUMN_HOSTNAME + "text not null" + ");";
+			
 	private static final String CREATE_USERTABLE = "Create table "
-			+ USER_TABLE + "(" + USER_COLUMNID
-			+ " integer primary key autoincrement, " + COLUMN_USERONE
-			+ " text not null, " + COLUMN_USERTWO
-			+ " text, " + COLUMN_USERTHREE
-			+ " text not null, " + COLUMN_NAME
-			+ " text, " + COLUMN_ADDRESS
-			+ " text, " + COLUMN_USERPASS
-			+ " text);";
+			+ USER_TABLE + "(" 
+			+ USER_COLUMNID + " integer primary key autoincrement, "
+			+ COLUMN_USERONE + " text not null, " 
+			+ COLUMN_USERTWO + " text, " 
+			+ COLUMN_USERTHREE + " text not null, " 
+			+ COLUMN_NAME + " text, " 
+			+ COLUMN_ADDRESS + " text, "
+			+ COLUMN_USERPASS + " text);";
 	
+	/**
+	 * Constructor for the database
+	 * @param context
+	 */
 	public WhircDB(Context context)
 	{
 		super(context, DATABASE_NAME , null, DATABASE_VERSION);
 	}
 	
+	
+	/**
+	 * 	function run on creation
+	 *  running sql-statements for
+	 *  creating tables
+	 *  
+	 *  @param database
+	 */
 	@Override
 	public void onCreate(SQLiteDatabase database)
 	{
@@ -57,6 +78,13 @@ public class WhircDB extends SQLiteOpenHelper {
 		database.execSQL(CREATE_USERTABLE);
 	}
 	
+	/**
+	 *  function run when the database is upgraded
+	 *  
+	 *  @param db
+	 *  @param oldVersion
+	 *  @param newVersion
+	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
 	{
