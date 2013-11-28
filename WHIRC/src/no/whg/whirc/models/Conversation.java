23049ad028e76ebb7af1f5d2a1d90fd705320207
Fredrik Pettersen <fredrik.fumler@gmail.com>
2013-11-28 21:51:54 +0100

    added proper timestamp, probably..

--- WHIRC/src/no/whg/whirc/models/Conversation.java
+++ WHIRC/src/no/whg/whirc/models/Conversation.java
@@ -1,6 +1,7 @@
 package no.whg.whirc.models;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import jerklib.Channel;
@@ -22,101 +23,107 @@ import android.widget.ListView;
  *
  */
 public class Conversation {
-	private ArrayList<Message> messages;
-	private Channel channel;
-	private ListView messageView;
-	private MainActivity activity;
-	private MessageAdapter messageAdapter;
-	private String channelTitle;
-	
-	public Conversation(Channel channel){
-		this.channel = channel;
-		this.channelTitle = channel.getName();
-		messages = new ArrayList<Message>();
-		Message topic = new Message("none", "no topic", "never", 0);
-		messages.add(topic);
-//		if (!channel.getTopic().equals("")){
-//			Message topic = new Message(channel.getTopicSetter(), channel.getTopic(), channel.getTopicSetTime().toString());
-//			messages.add(topic);
-//		}
-	}
-	
-	public Conversation(Channel channel, String priv){
-		this.channel = channel;
-		this.channelTitle = priv;
-		messages = new ArrayList<Message>();
-	}
-	
-	public Conversation(String servername){
-		this.channelTitle = servername;
-		
-		messages = new ArrayList<Message>();
-	}
-	
-	public ArrayList<Message> getMessages(){
-		return messages;
-	}
-	
-	public void addMessage(Message m){
-		messages.add(m);
-	}
-	
-	public void addMessage(MessageEvent me){
-		addMessage(new Message(me.getNick(), me.getMessage(), (new Date().toString()), me.hashCode()));
-	}
+    private ArrayList<Message> messages;
+    private Channel channel;
+    private ListView messageView;
+    private MainActivity activity;
+    private MessageAdapter messageAdapter;
+    private String channelTitle;
 
-	public void addMessage(MotdEvent me){
-		addMessage(new Message(me.getHostName(), me.getMotdLine(), (new Date().toString()), me.hashCode()));
-	}
-	
-	public void addMessage(TopicEvent te){
-		Message topic = new Message(te.getSetBy(), te.getTopic(), te.getSetWhen().toString(), 0);
-		messages.set(0, topic);
-	}
-	
-	public void addMessage(JoinEvent je){
-		addMessage(new Message(getChannelTitle(), "User " + je.getNick() + " joined the channel (" + je.getUserName() + " " + je.getHostName() + ").", (new Date().toString()), je.hashCode()));
-	}
-	
-	public void addMessage(PartEvent pe){
-		addMessage(new Message(getChannelTitle(), "User " + pe.getWho() + " left the channel (" + pe.getUserName() + " " + pe.getHostName() + "):\n" + pe.getPartMessage(), (new Date().toString()), pe.hashCode()));
-	}
-	
-	public void addMessage(KickEvent ke){
-		addMessage(new Message(getChannelTitle(), "User " + ke.getWho() + " was kicked from channel (" + ke.getUserName() + " " + ke.getHostName() + "):\n" + ke.getMessage(), (new Date().toString()), ke.hashCode()));
-	}
-	
-	public void addMessage(AwayEvent ae){
-		if (ae.isYou()){
-			if (ae.isAway()){
-				addMessage(new Message(ae.getSession().getConnectedHostName(), "You are now listed as AWAY.", (new Date().toString()), ae.hashCode()));
-			} else {
-				addMessage(new Message(ae.getSession().getConnectedHostName(), "You are no longer listed as AWAY.", (new Date().toString()), ae.hashCode()));
-			}
-		} else {
-			if (ae.isAway()){
-				addMessage(new Message(ae.getSession().getConnectedHostName(), ae.getNick() + " is now listed as AWAY (" + ae.getAwayMessage() + ").", (new Date().toString()), ae.hashCode()));
-			} else {
-				addMessage(new Message(ae.getSession().getConnectedHostName(), ae.getNick() + " is no longer listed as AWAY.", (new Date().toString()), ae.hashCode()));
-			}
-		}
-	}
-	
-	public void addMessage(ModeEvent me){
-		addMessage(new Message(me.setBy(), "Mode set to " + me.getModeAdjustments().toString(), (new Date().toString()), me.hashCode()));
-	}
-	
-	public String getChannelTitle(){
-		return channelTitle;
-	}
-	
-	public boolean hasMessage(int hash){
-		for (Message m : messages){
-			if (m.getHashcode() == hash){
-				return true;
-			}
-		}
-		
-		return false;
-	}
+    public Conversation(Channel channel){
+        this.channel = channel;
+        this.channelTitle = channel.getName();
+        messages = new ArrayList<Message>();
+        Message topic = new Message("none", "no topic", "never", 0);
+        messages.add(topic);
+//      if (!channel.getTopic().equals("")){
+//          Message topic = new Message(channel.getTopicSetter(), channel.getTopic(), channel.getTopicSetTime().toString());
+//          messages.add(topic);
+//      }
+    }
+
+    public Conversation(Channel channel, String priv){
+        this.channel = channel;
+        this.channelTitle = priv;
+        messages = new ArrayList<Message>();
+    }
+
+    public Conversation(String servername){
+        this.channelTitle = servername;
+
+        messages = new ArrayList<Message>();
+    }
+
+    public ArrayList<Message> getMessages(){
+        return messages;
+    }
+
+    public void addMessage(Message m){
+        messages.add(m);
+    }
+
+    public void addMessage(MessageEvent me){
+        addMessage(new Message(me.getNick(), me.getMessage(), getTime(), me.hashCode()));
+    }
+
+    public void addMessage(MotdEvent me){
+        addMessage(new Message(me.getHostName(), me.getMotdLine(), getTime(), me.hashCode()));
+    }
+
+    public void addMessage(TopicEvent te){
+        Message topic = new Message(te.getSetBy(), te.getTopic(), te.getSetWhen().toString(), 0);
+        messages.set(0, topic);
+    }
+
+    public void addMessage(JoinEvent je){
+        addMessage(new Message(getChannelTitle(), "User " + je.getNick() + " joined the channel (" + je.getUserName() + " " + je.getHostName() + ").", getTime(), je.hashCode()));
+    }
+
+    public void addMessage(PartEvent pe){
+        addMessage(new Message(getChannelTitle(), "User " + pe.getWho() + " left the channel (" + pe.getUserName() + " " + pe.getHostName() + "):\n" + pe.getPartMessage(), getTime(), pe.hashCode()));
+    }
+
+    public void addMessage(KickEvent ke){
+        addMessage(new Message(getChannelTitle(), "User " + ke.getWho() + " was kicked from channel (" + ke.getUserName() + " " + ke.getHostName() + "):\n" + ke.getMessage(), getTime(), ke.hashCode()));
+    }
+
+    public void addMessage(AwayEvent ae){
+        if (ae.isYou()){
+            if (ae.isAway()){
+                addMessage(new Message(ae.getSession().getConnectedHostName(), "You are now listed as AWAY.", getTime(), ae.hashCode()));
+            } else {
+                addMessage(new Message(ae.getSession().getConnectedHostName(), "You are no longer listed as AWAY.", getTime(), ae.hashCode()));
+            }
+        } else {
+            if (ae.isAway()){
+                addMessage(new Message(ae.getSession().getConnectedHostName(), ae.getNick() + " is now listed as AWAY (" + ae.getAwayMessage() + ").", getTime(), ae.hashCode()));
+            } else {
+                addMessage(new Message(ae.getSession().getConnectedHostName(), ae.getNick() + " is no longer listed as AWAY.", getTime(), ae.hashCode()));
+            }
+        }
+    }
+
+    public void addMessage(ModeEvent me){
+        addMessage(new Message(me.setBy(), "Mode set to " + me.getModeAdjustments().toString(), getTime(), me.hashCode()));
+    }
+
+    public String getChannelTitle(){
+        return channelTitle;
+    }
+
+    public boolean hasMessage(int hash){
+        for (Message m : messages){
+            if (m.getHashcode() == hash){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public String getTime() {
+        Calendar cal = Calendar.getInstance();
+
+        return cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
+    }
 }
