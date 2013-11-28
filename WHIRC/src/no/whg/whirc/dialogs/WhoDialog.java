d80f86e296d3412fbe48e2e6504c9e07d5937c3a
Inge Dalby <inge.dalby@gmail.com>
2013-11-28 23:12:15 +0100

    dialog_who.xml basically done

--- WHIRC/src/no/whg/whirc/dialogs/WhoDialog.java
+++ WHIRC/src/no/whg/whirc/dialogs/WhoDialog.java
@@ -1,7 +1,16 @@
 package no.whg.whirc.dialogs;
 
+import android.app.Dialog;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
 public class WhoDialog extends DialogFragment {
-	
+	public WhoDialog() {
+		// Set stuff here
+	}
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		return super.onCreateDialog(savedInstanceState);
+	}
 }
